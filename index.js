0746944258fbaf0aff008c59da258eb765ad1db1
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-08-20 16:57:31 -0400

    update

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const config = require('config')
 const main = async function() {
   const server = config.get('server')
   const dataset = config.get('dataset')
-  const t = config.get('t')
-  const shp = await fetch(`https://${server}/${dataset}/${t}.shp`)
-  const dbf = await fetch(`https://${server}/${dataset}/${t}.dbf`)
-  shapefile.open(shp.body, dbf.body, {encoding: 'utf-8'})
-    .then(source => source.read()
-      .then(function log(result) {
-        if (result.done) return
-        console.log(JSON.stringify(result.value))
-        return source.read().then(log)
-      }))
-    .catch(error => console.error(error.stack));
+  const layers = config.get('layers')
+  for (const layer of layers) {
+    const shp = await fetch(`https://${server}/${dataset}/${layer}.shp`)
+    const dbf = await fetch(`https://${server}/${dataset}/${layer}.dbf`)
+    shapefile.open(shp.body, dbf.body, {encoding: 'utf-8'})
+      .then(source => source.read()
+        .then(function log(result) {
+          if (result.done) return
+          console.log(JSON.stringify(result.value))
+          return source.read().then(log)
+        }))
+      .catch(error => console.error(error.stack));
+  }
 }
-
 main()
